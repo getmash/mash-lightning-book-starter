1459f997d4622bb3d7f7ae18efdd4eb1fd29b8f8
Mike Dedys <mdedys@users.noreply.github.com>
2022-09-02 14:16:14 -0400

    Update description

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -6,7 +6,7 @@ const config: GatsbyConfig = {
     shortTitle: "Mash Lightning Book",
     author: "Mike Dedys",
     description:
-      "This timely tale is packed with striking imagery and descriptions, highlighting how Bitcoin is upending everything we thought we knew about money, and drawing attention to issues plaguing our current monetary system.",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
     siteUrl: "https://loremipsumbook.com/",
     social: {
       twitter: "getmash",
